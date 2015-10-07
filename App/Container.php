6f428e075b9501a99e33dc88d6df9a3296928271
kriby <roma.liukshyn@gmail.com>
2015-10-07 19:41:27 +0300

    Container modifications

--- App/Container.php
+++ App/Container.php
@@ -3,13 +3,37 @@ namespace App;
 
 class Container
 {
-
+    /**
+     * @var array
+     */
     private $objects = [];
 
+    public function __construct()
+    {
+        $this->objects[get_class($this)] = $this;
+    }
+
+    /**
+     * Method returns an object of requested class. If object does not exist - creates it.
+     *
+     * @param $className
+     * @return mixed
+     */
     public function get($className)
     {
+        if(!isset($this->objects[$className])) {
+            $this->objects[$className] = $this->create($className);
+        }
         return $this->objects[$className];
     }
+
+
+    /**
+     * Method creates an instance of a requested class.
+     *
+     * @param $className
+     * @return object
+     */
     public function create($className)
     {
         $params = [];
@@ -21,18 +45,18 @@ class Container
         return $class->newInstanceArgs($params);
     }
 
+    /**
+     * Method returns an array with instances of requested arguments.
+     *
+     * @param $params
+     * @return array
+     */
     private function resolveArguments($params)
     {
         $result = [];
         if ($params) {
             foreach ($params as $param) {
-                $name = $param->getClass()->name;
-                if(isset($this->objects[$name])) {
-                    $result[] = $this->get($name);
-                } else {
-                    $this->objects[$name] = $this->create($name);
-                    $result[] = $this->get($name);
-                }
+                $result[] = $this->get($param->getClass()->name);
             }
         }
         return $result;
